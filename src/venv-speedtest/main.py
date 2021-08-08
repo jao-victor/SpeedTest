29104cf437248b9d787b9f700e529226577f217f
João Victor <ijoaovictor76@gmail.com>
2021-08-07 20:38:33 -0400

    .

---
+++ src/venv-speedtest/main.py
@@ -0,0 +1,17 @@
+# id no servidor da netware 39487
+# Coxim TJNET 11038
+# ideal gigarede anastácio 37365
+import speedtest
+
+servidores = [37365]
+
+threads = 32
+
+speed = speedtest.Speedtest()
+
+down = speed.download(threads=threads) 
+upld = speed.upload(threads=threads) 
+
+print(f'Download - {down}')
+print(f'Upload - {upld}')
+print('...')
